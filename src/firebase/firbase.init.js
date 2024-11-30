eedf46443425b2d9a4689ba20e399f947fa5a388
Sajnin14 <sajninsaima@gmail.com>
2024-11-30 18:09:38 +0600

    firebase install and setup done

---
+++ src/firebase/firbase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB36EkXt5sfjQq5HyKcNvyLVtepHHBqtgY",
+  authDomain: "coffee-master-daf1c.firebaseapp.com",
+  projectId: "coffee-master-daf1c",
+  storageBucket: "coffee-master-daf1c.firebasestorage.app",
+  messagingSenderId: "975917327544",
+  appId: "1:975917327544:web:37eb6c4818edeb13c4c04d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
